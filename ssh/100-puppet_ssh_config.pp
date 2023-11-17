b9d9a7a383821c9abbfb3f9c3027a5c7075db512
GChukwudi <g.chukwudi@alustudent.com>
2023-11-17 05:01:31 -0800

    client config

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# SSH configuration file to help connect to a server without typing a password
+
+file { 'etc/ssh/ssh_config':
+   ensure => present,
+content => "
+        Host *
+        IdentityFile ~/.ssh/school
+        PasswordAuthentication no
+"
+}

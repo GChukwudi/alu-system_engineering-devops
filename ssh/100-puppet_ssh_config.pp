c8a59f7858317e2198d20507fa060585e7bf000e
GChukwudi <g.chukwudi@alustudent.com>
2023-11-17 06:23:26 -0800

    client config

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -7,3 +7,9 @@ file { '':
   line   => '    PasswordAuthentication no',
   replace => true,
 }
+file { '':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+  replace => true,
+}
